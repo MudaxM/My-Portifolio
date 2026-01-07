5064e6735746b4cf0643647798d5876a28436333
MudaxM <mudinmt411@gmail.com>
2026-01-07 22:10:13 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
 
-const themeToggle = document.getElementById('theme-toggle');
-const currentTheme = localStorage.getItem('theme');
-
-// Check for saved user preference
-if (currentTheme) {
-  document.documentElement.setAttribute('data-theme', currentTheme);
-}
-
-themeToggle.addEventListener('click', () => {
-  let theme = document.documentElement.getAttribute('data-theme');
-  
-  if (theme === 'dark') {
-    document.documentElement.setAttribute('data-theme', 'light');
-    localStorage.setItem('theme', 'light');
-  } else {
-    document.documentElement.setAttribute('data-theme', 'dark');
-    localStorage.setItem('theme', 'dark');
-  }
-});
-
+const themeToggle = 
  document.querySelector('.menu-toggle');
 const navLinks = document.querySelector('.nav-links');
 
